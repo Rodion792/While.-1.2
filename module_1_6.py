e4fba88edb825f34009a7a973ec0793bbfb88d78
Rodion792 <duninrodion@gmail.com>
2024-11-14 00:41:30 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,11 @@
+my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+positive_numbers = []
+while index < len(my_list):
+    number = my_list[index]
+    if number < 0:
+        break
+    if number > 0:
+        positive_numbers.append(number)
+    index += 1
+print(positive_numbers)
